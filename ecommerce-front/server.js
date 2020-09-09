fe1bc77a46aadb238a1e481e765e2c4fd83c9c68
JamesYang <gn00999829@gmail.com>
2020-09-08 20:28:43 -0700

    try to deploy

---
+++ ecommerce-front/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.use(express.static(path.join(__dirname, 'build')));
+server.use('*', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+server.listen(PORT, () => console.log(`react running on ${PORT}...`));
